5f1b5e0af701b5c3b42b5a77cc3ebe34e85e462b
Anastasia Lukash <anastasia16rom@gmail.com>
2022-04-27 23:35:15 +0200

    check failed status

--- bin/brain-games.js
+++ bin/brain-games.js
@@ -3,4 +3,7 @@ import greeting from './cli.js';
 
 console.log('Welcome to the Brain Games!');
 console.log(`Hello, ${greeting}!`);
-console.log('Random string');
+console.log('Random string')
+
+
+
